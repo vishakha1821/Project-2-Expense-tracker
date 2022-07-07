34cbda3f50e96cbd4cf098001e1de2d898851d2a
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-07-07 19:28:32 +0530

    Add files via upload

---
+++ expense tracker project -2/script.js
@@ -0,0 +1,34 @@
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector('form-message');
+    messageElement.textContent = message;
+    messageElement.classList.remove('form-message-success', 'form-message-error');
+    messageElement.classList.add(`form-message ${type}`);
+}
+setFormMessage(loginForm, "success", 'you are loged in!');
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const loginForm = document.querySelector('#login');
+    const createAccountForm = document.querySelector('#createAccount');
+
+    document.querySelector('#linkCreateAccount').addEventListener('click', e =>{
+        e.preventDefault();
+        loginForm.classList.add('form-hidden');
+        createAccountForm.classList.remove('form-hidden')
+    });
+    document.querySelector('#linkLogin').addEventListener('click', e =>{
+        e.preventDefault();
+        loginForm.classList.remove('form-hidden');
+        createAccountForm.classList.add('form-hidden')
+    });
+});
+
+fetch('https://jsonplaceholder.typicode.com/todos/1', {
+   method: 'post',
+   body: JSON.stringify({
+    title:name,
+    password: password,
+    
+   })
+});
+
+axios.post('http://127.0.0.1:5500/index.html')
